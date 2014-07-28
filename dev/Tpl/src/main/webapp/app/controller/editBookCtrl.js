8d4cb2a16cb1fb3a4c33e6b9e0b30e29004d0daf
Federico Doll <fdoll@hexacta.com>
2014-07-28 17:44:08 -0300

    Commit

--- dev/Tpl/src/main/webapp/app/controller/editBookCtrl.js
+++ dev/Tpl/src/main/webapp/app/controller/editBookCtrl.js
@@ -21,7 +21,7 @@ booksApp.controller('editBookCtrl', function ($scope,$location,$rootScope,$route
     
     $http({
 		method : 'GET',
-		url: '/Tpl/rest/getBook/'+$scope.bookId,
+		url: '/Tpl/rest/books/'+$scope.bookId,
 		headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
 	}).success(function(data, status, headers, config){
 
