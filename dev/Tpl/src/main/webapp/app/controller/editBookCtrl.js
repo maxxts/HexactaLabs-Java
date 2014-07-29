db381e457b94d1504b6027662fcec2b44e17aeb8
Maximiliano J. Valverde <maxxts@gmail.com>
2014-07-29 14:50:02 -0300

    Book values in editPage

--- dev/Tpl/src/main/webapp/app/controller/editBookCtrl.js
+++ dev/Tpl/src/main/webapp/app/controller/editBookCtrl.js
@@ -1,6 +1,7 @@
 booksApp.controller('editBookCtrl', function ($scope,$location,$rootScope,$routeParams, $http) {
 	$scope.books = $rootScope.books;
 	
+	
 	$scope.backToHome = function(){
     	$location.path("/");
     };
@@ -19,15 +20,17 @@ booksApp.controller('editBookCtrl', function ($scope,$location,$rootScope,$route
     $scope.bookId = $routeParams.bookId;
     $scope.currentBook = null;
     
+    
     $http({
 		method : 'GET',
 		url: '/Tpl/rest/books/'+$scope.bookId,
 		headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
 	}).success(function(data, status, headers, config){
-
-		if(data.success)
+				
+		if(status = 200)
 		{
 			$scope.currentBook = data;
+			console.log("Book's Country: " + $scope.currentBook.country);
 		}
 
 	}).error(function(data, status, headers, config){
