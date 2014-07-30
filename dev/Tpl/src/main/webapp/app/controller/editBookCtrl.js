83573d93115c0b9909796d0c08023a77133bc50e
Maximiliano J. Valverde <maxxts@gmail.com>
2014-07-30 10:48:25 -0300

    Form Validations (Correction)

--- dev/Tpl/src/main/webapp/app/controller/editBookCtrl.js
+++ dev/Tpl/src/main/webapp/app/controller/editBookCtrl.js
@@ -1,39 +1,42 @@
-booksApp.controller('editBookCtrl', function ($scope,$location,$rootScope,$routeParams, $http) {
+booksApp.controller('editBookCtrl', function($scope, $location, $rootScope,
+		$routeParams, $http) {
 	$scope.books = $rootScope.books;
-	
-	
-	$scope.backToHome = function(){
-    	$location.path("/");
-    };
-    
-    $scope.save = function(aBook) {
-    	$scope.newBook = angular.copy(aBook);
-    	$rootScope.books[$rootScope.books.length] = aBook;
-    };
 
-    $scope.reset = function() {
-    	$scope.newBook = {};
-    };
+	$scope.backToHome = function() {
+		$location.path("/");
+	};
 
-    $scope.reset();
-    
-    $scope.bookId = $routeParams.bookId;
-    $scope.currentBook = null;
-    
-    
-    $http({
+	$scope.save = function(aBook) {
+		$scope.newBook = angular.copy(aBook);
+		$rootScope.books[$rootScope.books.length] = aBook;
+	};
+
+	$scope.reset = function() {
+		$scope.newBook = {};
+	};
+
+	$scope.reset();
+
+	$scope.bookId = $routeParams.bookId;
+	$scope.currentBook = null;
+
+	$http({
 		method : 'GET',
-		url: '/Tpl/rest/books/'+$scope.bookId,
-		headers : {'Content-type' : 'application/json', 'Accept' : 'application/json'}
-	}).success(function(data, status, headers, config){
-				
-		if(status = 200)
-		{
+		url : '/Tpl/rest/books/' + $scope.bookId,
+		headers : {
+			'Content-type' : 'application/json',
+			'Accept' : 'application/json'
+		}
+	}).success(function(data, status, headers, config) {
+
+		if (status = 200) {
 			$scope.currentBook = data;
 			console.log("Book's Country: " + $scope.currentBook.country);
 		}
 
-	}).error(function(data, status, headers, config){
-		console.log("An Error occurred while trying to get book:" + $scope.bookId);
-	});
+	}).error(
+			function(data, status, headers, config) {
+				console.log("An Error occurred while trying to get book:"
+						+ $scope.bookId);
+			});
 });
