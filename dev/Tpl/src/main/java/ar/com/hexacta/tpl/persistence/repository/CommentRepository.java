bbddacf635d9d84485f4e440347cc499bbc37224
Maximiliano J. Valverde <maxxts@gmail.com>
2014-07-30 14:28:33 -0300

    Comment Interfaces Created

---
+++ dev/Tpl/src/main/java/ar/com/hexacta/tpl/persistence/repository/CommentRepository.java
@@ -0,0 +1,25 @@
+package ar.com.hexacta.tpl.persistence.repository;
+
+import java.util.List;
+
+import ar.com.hexacta.tpl.model.Comment;
+
+public interface CommentRepository {
+
+	void save(final Comment comment);
+
+	// void update(final Comment book);
+
+	void delete(final Comment comment);
+
+	void deleteById(String commentId);
+
+	List<Comment> findAll();
+
+	Comment findById(String commentId);
+
+	List<Comment> findByBookId(String bookId);
+
+	// List<Comment> findByBook(Book book);
+
+}
---
+++ dev/Tpl/src/main/java/ar/com/hexacta/tpl/service/ICommentService.java
@@ -0,0 +1,25 @@
+package ar.com.hexacta.tpl.service;
+
+import java.util.List;
+
+import ar.com.hexacta.tpl.model.Comment;
+
+public interface ICommentService {
+
+	List<Comment> findAllComments();
+
+	List<Comment> findCommentsByBookId(String bookId);
+
+	// List<Comment> findCommentsByBook(Book book);
+
+	Comment findComment(String commentId);
+
+	void createComment(final Comment comment);
+
+	// void updateComment(final Comment comment);
+
+	void deleteComment(final Comment comment);
+
+	void deleteCommentById(String commentId);
+
+}
