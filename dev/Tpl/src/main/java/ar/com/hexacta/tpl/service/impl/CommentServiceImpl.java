3c9ad9140923fe300548c2027fd97e99af38c716
MatiasFito <matias.fito@outlook.com>
2014-08-01 14:20:15 -0300

    Cambios menores

--- dev/Tpl/src/main/java/ar/com/hexacta/tpl/service/impl/CommentServiceImpl.java
+++ dev/Tpl/src/main/java/ar/com/hexacta/tpl/service/impl/CommentServiceImpl.java
@@ -3,12 +3,14 @@ package ar.com.hexacta.tpl.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.com.hexacta.tpl.model.Comment;
 import ar.com.hexacta.tpl.persistence.repository.CommentRepository;
 import ar.com.hexacta.tpl.service.ICommentService;
 
+@Service
 public class CommentServiceImpl implements ICommentService {
 
     @Autowired
