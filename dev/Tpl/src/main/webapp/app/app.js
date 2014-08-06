cc60dfc6bca788ddfab6930a2366e7cdd88a5cf9
MatiasFito <matias.fito@outlook.com>
2014-08-06 14:44:22 -0300

    Estructura de la directiva "user login"
    Falta la lógica de validación que evite enviar el formulario en caso de
    que su contenido sea incorrecto, y el envío en formato Json.

--- dev/Tpl/src/main/webapp/app/app.js
+++ dev/Tpl/src/main/webapp/app/app.js
@@ -27,6 +27,38 @@ booksApp.config(['$routeProvider', '$locationProvider', function ($routeProvider
 		});
 }]);
 
+booksApp.directive('userLogin',function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'app/views/loginView.html',
+		controller: ['$scope', '$http', '$location', function($scope, $http, $location) {
+			
+		function test(nombre) {
+			alert(nombre);
+		}
+
+		  $scope.validateAndSubmit = function() {
+			  
+		  var name = $scope.user.name;
+		  var email = $scope.user.email;
+		  
+		  test(name);
+		  
+		  //Validacion de datos ingresados, despúes mandar al WS
+		  if( name == "asd") {
+			  $location.path("/ok");
+		  }
+		  
+		  //Dar mensajes de error
+		  else {
+	    	  console.log($scope.user.name);  
+		  }
+			  
+		  }
+	    }]
+	}
+});
+
 booksApp.run(function($rootScope,$http) {
 	$rootScope.books = [];
 });
